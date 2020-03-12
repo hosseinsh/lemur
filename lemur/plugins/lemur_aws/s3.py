8fc814b999f23f39cefa1245906106130f74979f
Hossein Shafagh <hshafagh@netflix.com>
2020-03-11 17:47:25 -0700

    adding S3 get and delete object  methods

--- lemur/plugins/lemur_aws/s3.py
+++ lemur/plugins/lemur_aws/s3.py
@@ -33,3 +33,23 @@ def put(bucket_name, region, prefix, data, encrypt, **kwargs):
         )
     else:
         bucket.put_object(Key=prefix, Body=data, ACL="bucket-owner-full-control")
+
+
+@sts_client("s3", service_type="client")
+def delete(bucket_name, prefix, **kwargs):
+    """
+    Use STS to delete an object
+    """
+    response = kwargs["client"].delete_object(Bucket=bucket_name, Key=prefix)
+    current_app.logger.debug(f"Delete data from S3. Bucket: {bucket_name} Prefix: {prefix}, Status_code: {response}")
+    return response['ResponseMetadata']['HTTPStatusCode'] == 204
+
+
+@sts_client("s3", service_type="client")
+def get(bucket_name, prefix, **kwargs):
+    """
+    Use STS to get an object
+    """
+    response = kwargs["client"].get_object(Bucket=bucket_name, Key=prefix)
+    current_app.logger.debug(f"Get data from S3. Bucket: {bucket_name} Prefix: {prefix}")
+    return response['Body']._raw_stream.data.decode("utf-8")
